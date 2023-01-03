a2f787bdab8c62692a40af88893f04b5a69c53be
Foxygene <foxygene.dev@gmail.com>
2023-01-03 23:24:50 +0100

    fix(Bills): order by time fixed

--- packages/billed-web/src/views/BillsUI.js
+++ packages/billed-web/src/views/BillsUI.js
@@ -1,31 +1,48 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from './VerticalLayout.js';
+import ErrorPage from './ErrorPage.js';
+import LoadingPage from './LoadingPage.js';
+import { formatDate } from '../app/format.js';
 
-import Actions from './Actions.js'
+import Actions from './Actions.js';
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
-      <td>${bill.date}</td>
+      <td>${formatDate(bill.date)}</td>
       <td>${bill.amount} €</td>
       <td>${bill.status}</td>
       <td>
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  return data && data.length
+    ? data
+        .sort((shotA, shotB) => {
+          const a = Date.parse(shotA.date);
+          const b = Date.parse(shotB.date);
+
+          if (a < b) {
+            return 1;
+          }
+          if (a > b) {
+            return -1;
+          }
+
+          return 0;
+        })
+        .map((bill) => row(bill))
+        .join('')
+    : '';
+};
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +57,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +92,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
