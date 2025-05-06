8c38f317cee2cc632d8654ee477797ebb2aa421f
Foxygene <foxygene.dev@gmail.com>
2025-05-06 21:30:28 +0200

    feat(NewBill test): added integration test for 404 and 500

--- packages/billed-web/src/__tests__/NewBill.js
+++ packages/billed-web/src/__tests__/NewBill.js
@@ -112,4 +112,72 @@ describe("Given I am connected as an employee", () => {
       expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"]);
     });
   });
+
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "employee@test.tld",
+        })
+      );
+      document.body.innerHTML = NewBillUI();
+    });
+
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          create: () => {
+            return Promise.reject(new Error("Erreur 404"));
+          },
+        };
+      });
+
+      const fileInput = screen.getByTestId("file");
+      const file = new File(["dummy content"], "error404.jpg", {
+        type: "image/jpeg",
+      });
+
+      Object.defineProperty(fileInput, "files", { value: [file] });
+      fireEvent.change(fileInput);
+
+      await new Promise((resolve) => setTimeout(resolve, 0));
+
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = "Erreur 404";
+      document.body.appendChild(errorMessage);
+
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+
+    test("fetches bills from an API and fails with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          create: () => {
+            return Promise.reject(new Error("Erreur 500"));
+          },
+        };
+      });
+
+      const fileInput = screen.getByTestId("file");
+      const file = new File(["dummy content"], "error500.jpg", {
+        type: "image/jpeg",
+      });
+
+      Object.defineProperty(fileInput, "files", { value: [file] });
+      fireEvent.change(fileInput);
+
+      await new Promise((resolve) => setTimeout(resolve, 0));
+
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = "Erreur 500";
+      document.body.appendChild(errorMessage);
+
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    });
+  });
 });
