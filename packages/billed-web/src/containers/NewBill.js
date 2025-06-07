183af46ea2745520c21d6c07b64ad1be39a35618
Foxygene <foxygene.dev@gmail.com>
2025-06-07 18:33:17 +0200

    fix(NewBills): quick bugfix on handlechangefile

--- packages/billed-web/src/containers/NewBill.js
+++ packages/billed-web/src/containers/NewBill.js
@@ -22,8 +22,7 @@ export default class NewBill {
     e.preventDefault();
     const fileInput = this.document.querySelector(`input[data-testid="file"]`);
     const file = fileInput.files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
+    const fileName = file.name; // ✅ Correction ici
 
     const allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
     if (!allowedExtensions.exec(fileName)) {
